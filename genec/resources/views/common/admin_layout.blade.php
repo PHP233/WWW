8181efc448cfafe1f986fd3711f510234df2b6c3
shaun2016 <1549118476@qq.com>
2017-09-21 22:13:37 +0800

    admin-index png and datatables style

--- genec/resources/views/common/admin_layout.blade.php
+++ genec/resources/views/common/admin_layout.blade.php
@@ -627,32 +627,6 @@ License: You must have a valid license purchased only from themeforest(the above
                         <span>Datatables</span>
                     </li>
                 </ul>
-                <div class="page-toolbar">
-                    <div class="btn-group pull-right">
-                        <button type="button" class="btn green btn-sm btn-outline dropdown-toggle" data-toggle="dropdown"> Actions
-                            <i class="fa fa-angle-down"></i>
-                        </button>
-                        <ul class="dropdown-menu pull-right" role="menu">
-                            <li>
-                                <a href="#">
-                                    <i class="icon-bell"></i> Action</a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <i class="icon-shield"></i> Another action</a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <i class="icon-user"></i> Something else here</a>
-                            </li>
-                            <li class="divider"> </li>
-                            <li>
-                                <a href="#">
-                                    <i class="icon-bag"></i> Separated link</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
             </div>
             <!-- END PAGE BAR -->
             <!-- BEGIN PAGE TITLE-->
@@ -1588,8 +1562,8 @@ License: You must have a valid license purchased only from themeforest(the above
 </div>
 <!-- END FOOTER -->
 <!--[if lt IE 9]>
-<scripts src="../assets/global/plugins/respond.min.js"></scripts>
-<scripts src="../assets/global/plugins/excanvas.min.js"></scripts>
+<scripts src="{{ asset('static/assets/plugins/respond.min.js')}}"></scripts>
+<scripts src="{{ asset('static/assets/plugins/excanvas.min.js')}}"></scripts>
 <![endif]-->
 <!-- BEGIN CORE PLUGINS -->
 <script src="{{ asset('static/assets/js/jquery.min.js') }}" type="text/javascript"></script>
