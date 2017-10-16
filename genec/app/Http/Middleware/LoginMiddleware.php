7f8e9e425e8a3664bd2547b855ad36de9e273c69
shaun2016 <1549118476@qq.com>
2017-10-16 10:47:53 +0800

    wordpress redirect too many ?

--- genec/app/Http/Middleware/LoginMiddleware.php
+++ genec/app/Http/Middleware/LoginMiddleware.php
@@ -17,6 +17,9 @@ class LoginMiddleware {
 	 * 登录控制器：如果没有登录进行登录
 	 */
 	public function handle($request, Closure $next) {
+		if($request->is('wordpress/*') || $request->is('wordpress')) {
+			return $next($request);
+		}
 		if($request->is('reviewer/*')) {
 			if(!session('reviewer')) {
 				return redirect()->route('reviewer_login');
